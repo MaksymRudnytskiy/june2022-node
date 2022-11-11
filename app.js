cc065c6750b3eacecba6a209eda9f0ec84f9ceba
MaksymRudnytskiy <therudmax@gmail.com>
2022-11-11 10:09:00 +0200

    lesson1

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
 const fs = require('node:fs')
-// const sort = require('./students/sortStudents')
+const sort = require('./students/sortStudents')
+const {maleSort, femaleSort} = require("./students/sortStudents");
 
-fs.readdir('./students/female', {withFileTypes: true}, (err, files) => {
+maleSort()
+femaleSort()
 
-    for (const file of files) {
-
-        fs.readFile(`./students/female/${file.name}`, (err1, data) => {
-            const student = JSON.parse(data)
-
-            if (student.gender === "male") {
-
-                fs.rename(`./students/female/${file.name}`, `./students/male/${file.name}`, err2 => {
-                    console.log(err2);
-                })
-            }else if (student.gender === "female") {
-
-                fs.rename(`./students/male/${file.name}`, `./students/female/${file.name}`, err3 => {
-                    console.log(err3);
-
-                })
-            }
-        })
-    }
-})
 
 
 
--- students/sortStudents.js
+++ students/sortStudents.js
@@ -1,9 +1,44 @@
 const fs = require("fs");
 
-function studentsSort() {
-    fs.readdir()
+function femaleSort() {
+    fs.readdir('./students/female', {withFileTypes: true}, (err, files) => {
+
+        for (const file of files) {
+
+            fs.readFile(`./students/female/${file.name}`, (err1, data) => {
+                const student = JSON.parse(data)
+
+                if (student.gender === "male") {
+
+                    fs.rename(`./students/female/${file.name}`, `./students/male/${file.name}`, err2 => {
+                        console.log(err2);
+                    })
+                }
+            })
+        }
+    })
+}
+
+function maleSort() {
+    fs.readdir('./students/male', {withFileTypes: true}, (err, files) => {
+
+        for (const file of files) {
+
+            fs.readFile(`./students/male/${file.name}`, (err1, data) => {
+                const student = JSON.parse(data)
+
+                if (student.gender === "female") {
+
+                    fs.rename(`./students/male/${file.name}`, `./students/female/${file.name}`, err2 => {
+                        console.log(err2);
+                    })
+                }
+            })
+        }
+    })
 }
 
 module.exports = {
-    studentsSort
+    femaleSort,
+    maleSort
 }
