9ddd668ef80f1db5ee5cb7d1b75585bb2b501cda
MaksymRudnytskiy <therudmax@gmail.com>
2022-11-19 22:46:29 +0200

    lesson 2 +

--- app.js
+++ app.js
@@ -1,40 +1,98 @@
 const express = require('express')
+const fs = require('fs/promises')
+const path = require("path");
 
-const userDb = require('./dataBase/users')
+// const userDb = require('./dataBase/users')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
     console.log('user endpoint')
-    res.json(userDb)
+
+    const userDb = await fs.readFile(path.join(__dirname, 'dataBase', 'users.json'))
+    const users = JSON.parse(userDb.toString())
+
+    res.json(users)
 })
 
-app.get('/users/:userId', (req, res) => {
+app.get('/users/:userId', async (req, res) => {
 
     const {userId} = req.params
+    const userDb = await fs.readFile(path.join(__dirname, 'dataBase', 'users.json'))
+    const users = JSON.parse(userDb.toString())
+    const findUser = users.find((user) => user.id === +userId)
 
-    res.json(userDb[userId])
+    if (!findUser) {
+        return res.json('ID not found')
+    }
+
+    res.json(findUser)
 })
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const userInfo = req.body
 
-    userDb.push(userInfo)
+    if (!userInfo.name || typeof userInfo.name === "string") {
+        return res.status(400).json('Invalid User')
+    }
+    if (userInfo.age < 0 || Number.isNaN(+userInfo.age)) {
+        return res.status(400).json('Invalid age')
+    }
 
-    res.status(201).json('created')
+    const userDb = await fs.readFile(path.join(__dirname, 'dataBase', 'users.json'))
+    const users = JSON.parse(userDb.toString())
+    const newUser = {
+        name: userInfo.name,
+        age: userInfo.age,
+        id: users[users.length - 1].id + 1}
+
+    users.push(newUser)
+
+    await fs.writeFile(path.join(__dirname, 'dataBase', 'users.json'), JSON.stringify(users))
+
+    res.status(201).json(newUser)
 })
 
-app.put('/users/:userId',(req, res) => {
+app.put('/users/:userId', async (req, res) => {
 
     const newUserInfo = req.body
-    const userId = req.params.userId
+    const {userId} = req.params
+    const userDb = await fs.readFile(path.join(__dirname, 'dataBase', 'users.json'))
+    const users = JSON.parse(userDb.toString())
+    const findId = users.findIndex((user) => user.id === +userId)
 
-    userDb[userId] = newUserInfo
+    if (findId === -1) {
+        return res.json('ID not found')
+    }
 
-    res.json('update')
+    users[findId] = {...users[findId], ...newUserInfo}
+    users.push(newUserInfo)
+
+    await fs.writeFile(path.join(__dirname, 'dataBase', 'users.json'), JSON.stringify(users))
+
+    res.json(users[findId])
+})
+
+app.delete('/users/:userId', async (req, res) => {
+
+    const {userId} = req.params
+    const userDb = await fs.readFile(path.join(__dirname, 'dataBase', 'users.json'))
+    const users = JSON.parse(userDb.toString())
+    const findId = users.findIndex((user) => user.id === +userId)
+
+    if (findId === -1) {
+        return res.json('ID not found')
+    }
+
+
+    users.splice(findId, 1)
+
+    await fs.writeFile(path.join(__dirname, 'dataBase', 'users.json'), JSON.stringify(users))
+
+    res.sendStatus(204)
 })
 
 app.get('/', (req, res) => {
